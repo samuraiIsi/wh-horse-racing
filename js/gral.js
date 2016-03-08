4aecbc7b5d8576ea9285e95a701096023d53da02
Isidoro Martinez <isidoro.martinez@petfre.gi>
2016-03-08 08:05:04 +0100

    Starting project

---
+++ js/gral.js
@@ -0,0 +1,9 @@
+// toggle with js
+
+// http://stackoverflow.com/questions/20997687/javascript-hiding-and-showing-div-tag-with-a-toggle-button
+
+// http://jsfiddle.net/fyUJc/40/
+
+// https://www.smashingmagazine.com/2009/04/showcase-of-well-designed-tabbed-navigation/
+
+// https://www.smashingmagazine.com/2009/03/designing-drop-down-menus-examples-and-best-practices/
